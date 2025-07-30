4b3777c0befac4f0760f04a10726052d0f034eab
Ross Evans <rpevans@chippie.chippie-domain.cs.uwaterloo.ca>
2025-07-30 01:26:32 +0000

    Stalking paper simulations file

--- simulations/paper_simulations.py
+++ simulations/paper_simulations.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     all_configs = []
 
     config = get_default_config()
-
+    '''
     # Baseline simulation:
     config.POSTBLACKOUT_DAYS = list(range(8, 61))
     config.REQUEST_CUTOFF_DAY = 15
@@ -39,18 +39,20 @@ if __name__ == "__main__":
         config.NUM_REGULAR = proactive
         config.NUM_LEECH = leech
         all_configs.append(ConfigNamePair(config, f"varyProactive/{i}"))
-
+    '''
+    '''
     # Varying Adversaries
     config = get_default_config()
-    make_dir("data/runs/varyAdversary")
+    make_dir("data/runs/varyAdversary-asatisfy")
     for i in [0, 0.01, 0.02, 0.05, 0.1, 0.25]:
         adversary, rest = split_by_percent(i, 25000)
         proactive, leech = split_by_percent(0.25, rest)
         config.NUM_ADVERSARY = adversary
         config.NUM_REGULAR = proactive
         config.NUM_LEECH = leech
-        all_configs.append(ConfigNamePair(config, f"varyAdversary/{i}"))
-
+        all_configs.append(ConfigNamePair(config, f"varyAdversary-asatisfy/{i}"))
+    '''
+    '''
     # Jamming Post-Blackout (without any adversary rating manipulation)
     config = get_default_config()
     make_dir("data/runs/jammingNoAdversary")
@@ -61,14 +63,25 @@ if __name__ == "__main__":
     # Jamming with adversary
     config = get_default_config()
     make_dir("data/runs/jammingWithAdversary")
-    for i in [0, 10, 100, 1000, 10000, 20000]:
-        adversary, rest = split_by_percent(0.02, 25000)
-        proactive, leech = split_by_percent(0.25, rest)
-        config.NUM_ADVERSARY = adversary
-        config.NUM_REGULAR = proactive
-        config.NUM_LEECH = leech
-        config.JAM_TOP_K_LOCATIONS = i
-        all_configs.append(ConfigNamePair(config, f"jammingWithAdversary/{i}"))
+    for i in [0, 0.01, 0.02, 0.05, 0.1, 0.25]:
+        for jam in [0, 10, 100, 1000, 10000, 20000]:
+            adversary, rest = split_by_percent(i, 25000)
+            proactive, leech = split_by_percent(0.25, rest)
+            config.NUM_ADVERSARY = adversary
+            config.NUM_REGULAR = proactive
+            config.NUM_LEECH = leech
+            config.JAM_TOP_K_LOCATIONS = jam
+            all_configs.append(ConfigNamePair(config, f"jammingWithAdversary/{i}-{jam}"))
+
+    # Cache start up time
+    config = get_default_config()
+    make_dir("data/runs/cacheStartUp")
+    for startup_days in [1, 3, 5, 7, 9, 11, 13]:
+        config.PREBLACKOUT_DAYS = list(range(1, startup_days + 1))
+        config.POSTBLACKOUT_DAYS = list(range(startup_days + 1, startup_days + 8))
+        config.REQUEST_CUTOFF_DAY = startup_days + 2
+        all_configs.append(ConfigNamePair(config, f"cacheStartUp/{startup_days}"))
+
 
     # Different contact probabilities
     config = get_default_config()
@@ -89,11 +102,12 @@ if __name__ == "__main__":
             )
 
     # EPIDEMIC
-
     config = get_default_config()
     make_dir("data/runs/epidemic")
-    for adversaryPercent in [0, 0.01, 0.02, 0.05, 0.1]:
-        for spamMultiplier in [1, 2, 4, 8, 16, 32, 64]:
+    for adversaryPercent in [0, 0.01, 0.02, 0.05, 0.1, 0.25]:
+        for spamMultiplier in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]:
+            if adversaryPercent == 0 and spamMultiplier != 1:
+                continue
             adversary, rest = split_by_percent(adversaryPercent, 25000)
             proactive, leech = split_by_percent(0.25, rest)
             config.NUM_ADVERSARY = adversary
@@ -102,14 +116,13 @@ if __name__ == "__main__":
             config.FETCHING_TYPE = FetchingType.EPIDEMIC
             config.ADVERSARY_FORCE_MULTIPLIER = spamMultiplier
             all_configs.append(
-                ConfigNamePair(config, f"epidemic/{adversaryPercent}-{spamMultiplier}")
-            )
+                ConfigNamePair(config, f"epidemic/{adversaryPercent}-{spamMultiplier}"))
 
     config = get_default_config()
     make_dir("data/runs/grid")
     for adversaryPercent in [0, 0.01, 0.02, 0.05, 0.1, 0.25]:
         for jam in [0, 10, 50, 100, 300]:
-            adversary, rest = split_by_percent(adversaryPercent, 25000)
+            adversary, rest = split_by_percent(adversaryPercent, 600)
             proactive, leech = split_by_percent(0.25, rest)
             config.SIMULATION_TYPE = ModelType.GRID
             config.NUM_ADVERSARY = adversary
@@ -118,8 +131,23 @@ if __name__ == "__main__":
             config.TOTAL_USERS = 600
             config.GRID_SIZE = 25
             config.JAM_TOP_K_LOCATIONS = jam
-            all_configs.append(ConfigNamePair(config, f"grid/a{adversaryPercent}"))
+            all_configs.append(ConfigNamePair(config, f"grid/a{adversaryPercent}-j{jam}"))'''
+
+    config = get_default_config()
+    make_dir("data/runs/stalking")
+    for adversaryPercent in [0, 0.01, 0.02, 0.05, 0.1, 0.25]:
+        for pow_limit in [30, 60, 90, 120, 360, 720, 1800]:
+            config.STALKING = True
+            adversary, rest = split_by_percent(adversaryPercent, 25000)
+            proactive, leech = split_by_percent(0.25, rest)
+            config.NUM_ADVERSARY = adversary
+            config.NUM_LEECH = leech
+            config.NUM_REGULAR = proactive
+            config.POW_LIMIT = pow_limit
+            all_configs.append(ConfigNamePair(config, f"stalking/{adversaryPercent}-{pow_limit}"))
+
     print("Created all configs")
+    print(len(all_configs))
     processes = []
     for config in all_configs:
         config_dir_name = f"data/runs/{config.name}"
@@ -134,7 +162,7 @@ if __name__ == "__main__":
             print("STARTING")
             print(config.config)
             processes.append(proc)
-        while len(processes) >= 4:
+        while len(processes) >= 224:
             for process in processes:
                 if process.poll() is not None:
                     processes.remove(process)
